b4860cd673906b447a76f8531ba1b8eb39e1848d
Alberto Cantallops <acantallops@mercari.com>
2020-08-18 23:06:32 +0900

    Initial Commit

---
+++ Shared/LighterPackApp.swift
@@ -0,0 +1,17 @@
+//
+//  LighterPackApp.swift
+//  Shared
+//
+//  Created by acantallops on 2020/08/18.
+//
+
+import SwiftUI
+
+@main
+struct LighterPackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
